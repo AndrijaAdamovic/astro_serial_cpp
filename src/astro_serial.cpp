ab272bf33ab8225be54f105cdb9057139299a03e
Andrija Adamovic <andrija.adamov@gmail.com>
2024-05-24 15:35:25 +0200

    modified:   src/astro_serial.cpp

--- src/astro_serial.cpp
+++ src/astro_serial.cpp
@@ -11,7 +11,7 @@
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "std_msgs/msg/float32.hpp"
-#include "std_srvs/srv/empty.hpp"
+#include "std_srvs/srv/trigger.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
@@ -55,10 +55,15 @@ public:
     joint_states_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", 10);
     current_publisher_ = this->create_publisher<std_msgs::msg::Float32>("/absoulte_current", 10);
 
-    if (this->get_parameter("publish_odom").as_bool()) odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
-    if (this->get_parameter("publish_odom_tf").as_bool() && this->get_parameter("publish_odom").as_bool()) odom_tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    if (this->get_parameter("publish_odom").as_bool())
+      odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+    if (this->get_parameter("publish_odom_tf").as_bool() && this->get_parameter("publish_odom").as_bool())
+      odom_tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
     odom_joint_states_timer_ = this->create_wall_timer(std::chrono::duration<double>((double)(1 / this->get_parameter("odom_joint_states_pub_freq").as_double())), std::bind(&AstroSerialNode::odom_joint_states_publish_callback, this));
-  
+
+    reset_odom_srv_ = this->create_service<std_srvs::srv::Trigger>("reset_odometry", std::bind(&AstroSerialNode::reset_odometry, this, std::placeholders::_1, std::placeholders::_2));
+
     cmd_vel_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&AstroSerialNode::cmd_vel_callback, this, std::placeholders::_1));
 
     RCLCPP_INFO(this->get_logger(), "Started astro_serial node!");
@@ -76,30 +81,39 @@ public:
     {
       serial_conn_.Close();
     }
-
+    
     RCLCPP_INFO(this->get_logger(), "ASTRO Serial Node is being destroyed");
   }
 
 private:
-
   BaudRate convert_baud_rate(int baud_rate)
   {
     switch (baud_rate)
     {
-      case 1200: return BaudRate::BAUD_1200;
-      case 1800: return BaudRate::BAUD_1800;
-      case 2400: return BaudRate::BAUD_2400;
-      case 4800: return BaudRate::BAUD_4800;
-      case 9600: return BaudRate::BAUD_9600;
-      case 19200: return BaudRate::BAUD_19200;
-      case 38400: return BaudRate::BAUD_38400;
-      case 57600: return BaudRate::BAUD_57600;
-      case 115200: return BaudRate::BAUD_115200;
-      case 230400: return BaudRate::BAUD_230400;
-      default:
-        RCLCPP_WARN(this->get_logger(), "Error! Baud rate %d not supported, use standard baud rates! Default to 57600", baud_rate);
+    case 1200:
+      return BaudRate::BAUD_1200;
+    case 1800:
+      return BaudRate::BAUD_1800;
+    case 2400:
+      return BaudRate::BAUD_2400;
+    case 4800:
+      return BaudRate::BAUD_4800;
+    case 9600:
+      return BaudRate::BAUD_9600;
+    case 19200:
+      return BaudRate::BAUD_19200;
+    case 38400:
+      return BaudRate::BAUD_38400;
+    case 57600:
+      return BaudRate::BAUD_57600;
+    case 115200:
+      return BaudRate::BAUD_115200;
+    case 230400:
+      return BaudRate::BAUD_230400;
+    default:
+      RCLCPP_WARN(this->get_logger(), "Error! Baud rate %d not supported, use standard baud rates! Default to 57600", baud_rate);
 
-        return BaudRate::BAUD_57600;
+      return BaudRate::BAUD_57600;
     }
   }
 
@@ -124,7 +138,7 @@ private:
     current_publisher_->publish(current_msg);
   }
 
-  void publish_odometry()
+  void publish_odometry(bool reset = false)
   {
     odom_current_time_ = this->get_clock()->now().seconds();
 
@@ -135,7 +149,7 @@ private:
     double velY = 0;
     double velTh = (this->get_parameter("wheel_radius").as_double() / this->get_parameter("wheel_separation").as_double()) * (wR - wL);
 
-    double dt = odom_current_time_ - odom_last_time_;
+    double dt = reset ? 0 : odom_current_time_ - odom_last_time_;
     double delta_x = (velX * cos(odom_th_)) * dt;
     double delta_y = (velY * sin(odom_th_)) * dt;
     double delta_th = velTh * dt;
@@ -172,15 +186,42 @@ private:
     odom_trans.transform.translation.z = 0.0;
     odom_trans.transform.rotation = tf2::toMsg(q);
 
-    if((this->get_parameter("publish_odom_tf").as_bool())) odom_tf_broadcaster_->sendTransform(odom_trans);
+    if ((this->get_parameter("publish_odom_tf").as_bool()))
+      odom_tf_broadcaster_->sendTransform(odom_trans);
 
     odom_last_time_ = odom_current_time_;
+  }
+
+  void reset_odometry(const std::shared_ptr<std_srvs::srv::Trigger::Request> request, std::shared_ptr<std_srvs::srv::Trigger::Response> response)
+  {
+    (void)request;
+    RCLCPP_INFO(this->get_logger(), "Incoming request: reset_odometry");
+
+    response->success = true;
+    response->message = "Odometry reset successful!";
+
+    try
+    {
+      RCLCPP_INFO(this->get_logger(), "Resetting odometry");
+      odom_x_ = 0;
+      odom_y_ = 0;
+      odom_th_ = 0;
+      publish_odometry(true);
+    }
+    catch (std::exception &e)
+    {
+      response->success = false;
+      response->message = "Reset odometry service failed";
+    }
+
+    RCLCPP_INFO(this->get_logger(), "Response: odometry was reset");
   }
 
   void odom_joint_states_publish_callback()
   {
     publish_joint_states();
-    if (this->get_parameter("publish_odom").as_bool()) publish_odometry();
+    if (this->get_parameter("publish_odom").as_bool())
+      publish_odometry();
   }
 
   void read_joint_states()
@@ -210,7 +251,6 @@ private:
         }
       }
     }
-
   }
 
   std::vector<std::string> split_string(const std::string &str, char delimiter)
@@ -229,7 +269,7 @@ private:
 
   double clamp_symetric(double value, double max_abs)
   {
-    if(value > max_abs)
+    if (value > max_abs)
     {
       value = max_abs;
     }
@@ -251,7 +291,7 @@ private:
 
     w.at(0) = clamp_symetric((twist_msg.linear.x / wheelRadius) - 0.5 * twist_msg.angular.z * (wheelSeparation / wheelRadius), maxWheelAngularVelocity);
     w.at(1) = clamp_symetric((twist_msg.linear.x / wheelRadius) + 0.5 * twist_msg.angular.z * (wheelSeparation / wheelRadius), maxWheelAngularVelocity);
-  
+
     return w;
   }
 
@@ -262,22 +302,22 @@ private:
       std::string velcity_command = "v " + std::to_string(w.at(0)) + " " + std::to_string(w.at(1)) + "\n";
       serial_conn_.Write(velcity_command);
     }
-    catch(const NotOpen&)
+    catch (const NotOpen &)
     {
       RCLCPP_ERROR(this->get_logger(), "UART port is not opened");
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
       RCLCPP_ERROR(this->get_logger(), "Error writing to UART: %s", e.what());
     }
   }
 
-  void cmd_vel_callback(const geometry_msgs::msg::Twist & msg)
+  void cmd_vel_callback(const geometry_msgs::msg::Twist &msg)
   {
     std::vector<double> w = diff_drive_inv_kinematics(msg);
     send_velocity(w);
   }
-  
+
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_publisher_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr current_publisher_;
   rclcpp::TimerBase::SharedPtr odom_joint_states_timer_;
@@ -296,6 +336,8 @@ private:
 
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> odom_tf_broadcaster_;
+
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_odom_srv_;
 
   std::thread uart_thread_;
   std::atomic<bool> uart_thread_running_;
